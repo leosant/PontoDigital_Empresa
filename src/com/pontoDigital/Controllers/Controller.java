235055fcc3708bcd365a9ef8db07a10808b3fe8a
leosant <leonardosantos45548@gmail.com>
2020-12-28 01:45:50 -0300

    Criando o desenho da segunda tela

--- src/com/pontoDigital/Controllers/Controller.java
+++ src/com/pontoDigital/Controllers/Controller.java
@@ -3,12 +3,15 @@ package com.pontoDigital.Controllers;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.swing.JOptionPane;
+
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -19,9 +22,13 @@ public class Controller{
     SimpleDateFormat sfd1 = new SimpleDateFormat("HH:mm:ss");
     SimpleDateFormat sfd2 = new SimpleDateFormat("dd/MM/yyyy");
     Date data = new Date();
+    
     //Buttons and label´s
     @FXML
-    private Button buttonPonto;
+    private Button btPonto;
+    @FXML
+    private TextField txtLogin;
+    
     @FXML
     private Label lblRelogio;
     @FXML
@@ -30,12 +37,14 @@ public class Controller{
     private Label lblData;
     @FXML
     private Label lblDataCompleta;
+    
     @FXML
     private ImageView setaPonto;
     @FXML
     private ImageView setaRelatorio;
     @FXML
     private ImageView setaSair;
+    
     //AnchorPanel
     @FXML
     private AnchorPane relogioPainel;
@@ -57,6 +66,10 @@ public class Controller{
 		timeline.setCycleCount(Timeline.INDEFINITE);
 		timeline.play();
     }
+    
+    public void registrarPonto() {
+    	JOptionPane.showMessageDialog(null, "Construindo o registro");
+    }
 
     public void botaoRelatorioPonto() {
         //True
@@ -66,6 +79,10 @@ public class Controller{
         setaSair.setVisible(false);
         setaPonto.setVisible(false);
         relogioPainel.setVisible(false);
+    }
+    
+    public void eventAcessarBD () {
+    	JOptionPane.showMessageDialog(null, "Construindo o acesso do banco de dados");
     }
     
     private void atualizaHoras() {
