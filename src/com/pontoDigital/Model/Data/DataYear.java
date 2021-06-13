265870a611b63afde700c6091973bbfc27989697
leona <leona@DESKTOP-N3EKO3U>
2021-06-12 22:31:54 -0300

    commit alter configuration of button touch dot

--- src/com/pontoDigital/Model/Data/DataYear.java
+++ src/com/pontoDigital/Model/Data/DataYear.java
@@ -1,36 +1,47 @@
 package com.pontoDigital.Model.Data;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
+import java.time.LocalTime;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.pontoDigital.Model.Employer.Funcionario;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Data
 @Table(name = "data_year")
 public class DataYear {
 	
+	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int id;
+	private Integer id;
 	
 	private Integer year;
 	
-	@OneToMany(mappedBy = "dataYear")
-	private List<DataMonth> dataMonth;
+	private Integer month;
 	
-	@OneToOne(cascade = CascadeType.ALL)
+	private Integer days;
+	
+	private LocalTime earlyHours;
+	
+	private LocalTime entranceLunch;
+	
+	private LocalTime exitLunch;
+	
+	private LocalTime finalHours;
+	
+	private Integer resultHours;
+	
+	@ManyToOne
 	@JoinColumn(name = "funcionario_fk")
-	private Funcionario funcionarios;
+	private Funcionario funcionarios;	
 }
--- src/com/pontoDigital/Service/StringUtilsOne.java
+++ src/com/pontoDigital/Service/StringUtilsOne.java
@@ -1,6 +1,9 @@
 package com.pontoDigital.Service;
 
+import java.time.LocalTime;
+
 import com.pontoDigital.DAO.InteractionDAO;
+import com.pontoDigital.Model.Data.DataYear;
 import com.pontoDigital.Model.Employer.Funcionario;
 
 import javafx.scene.control.TextField;
@@ -19,6 +22,36 @@ public class StringUtilsOne {
 		}
 		
 		return acessoFunc;
+	}
+	
+	public static void configurationOptionsAuto(DataYear dataYear) {
+		
+		if((LocalTime.now().isAfter(LocalTime.of(7, 59, 0)) &&
+				(LocalTime.now().isBefore(LocalTime.of(9, 59, 0))))) {
+			
+			System.out.println("Salvando na hora inicio");
+			dataYear.setEarlyHours(LocalTime.now());				
+		}
+		
+		if((LocalTime.now().isAfter(LocalTime.of(10, 0, 0)))
+				&& (LocalTime.now().isBefore(LocalTime.of(13, 29, 0)))) {
+			
+			System.out.println("Salvando no horário de almoço");
+			dataYear.setEntranceLunch(LocalTime.now());
+		}
+		
+		if((LocalTime.now().isAfter(LocalTime.of(13, 30, 0)))
+				&& (LocalTime.now().isBefore(LocalTime.of(14, 30, 0)))) {
+			
+			System.out.println("Salvando na saída do almoço");
+			dataYear.setExitLunch(LocalTime.now());
+		}
+		
+		else {
+			
+			System.out.println("Salvando na saída");
+			dataYear.setFinalHours(LocalTime.now());
+		}
 	}
 	
 }
